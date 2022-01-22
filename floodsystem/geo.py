11d087be3e76a26cfdf2595d584df37408b27217
iris23m <im514@cam.ac.uk>
2022-01-22 19:42:11 +0000

    Added a function in submodule geo to return a list of stations and their distance from a point

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,24 @@ geographical data.
 
 """
 
+import importlib
 from .utils import sorted_by_key  # noqa
+
+
+def stations_by_distance(stations, p):
+    "Takes a list of station data in the MonitoringStation form, and a coordinate and returns a list of tuples of the station name and its distance from that point."
+    from haversine import haversine, Unit
+    l = []
+    for e in stations:
+        Station = None
+        if "station_id" in e:
+            Station = e["station_id"]
+        Coord = None
+        if "coord" in e:
+            Coord = e["coord"]
+        Distance = haversine(Coord,p)
+        p = (Station, Distance)
+        l.append(p)
+    return sorted_by_key(l)
+
+
