41ac60c4ba95decb09162f81f78214bd26ae5653
iris23m <im514@cam.ac.uk>
2022-02-13 23:25:41 +0000

    adding in a flood submodule for task 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -19,3 +19,24 @@ def stations_level_over_threshold(stations, tol):
             list_of_tuples.append(station_and_level)
 
     return sorted_by_key(list_of_tuples,1)
+
+def stations_highest_rel_level(stations, N):
+    'returns a list of the N stations objects at which the water level is highest, in descending order by relative level'
+    from floodsystem.station import MonitoringStation
+    from floodsystem.stationdata import update_water_levels
+    from floodsystem.utils import sorted_by_key
+    update_water_levels(stations)
+    station_and_level = []
+    for station in stations:
+        level = MonitoringStation.relative_water_level(station)
+        
+        if level != None:
+            station_and_level.append((station,level))
+  
+    sorted_by_key(station_and_level, 1, True )
+    new_list = station_and_level[:N]
+    list_of_stations = []
+    for z in new_list:
+        list_of_stations.append(z[0])
+    
+    return list_of_stations
