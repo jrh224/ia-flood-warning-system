4a4d26df53c18edb07729b18b96486493f46914e
iris23m <im514@cam.ac.uk>
2022-02-13 23:32:44 +0000

    adding in tests for the submodule

--- test_flood.py
+++ test_flood.py
@@ -1,5 +1,7 @@
+from floodsystem.station import MonitoringStation
 from floodsystem.stationdata import build_station_list
 from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_highest_rel_level
 
 
 def test_stations_level_over_threshold():
@@ -8,6 +10,14 @@ def test_stations_level_over_threshold():
     assert len(testdata) > 0
     assert type(testdata[1]) == tuple
     assert type(testdata[1][1]) == float
+
+def test_stations_highest_rel_level():
+    stations = build_station_list()
+    test_data = stations_highest_rel_level(stations, 11)
+    assert len(test_data) == 11
+    assert MonitoringStation.relative_water_level(test_data[0]) > MonitoringStation.relative_water_level(test_data[1])
+    
+
 
 
 
