0708c873a95c5e7c541fd125f2488cd51c5eaffc
jrh224 <jrh224@cam.ac.uk>
2022-02-14 15:34:20 +0000

    added an incomplete test for the plot water levels function

---
+++ test_plot.py
@@ -0,0 +1,17 @@
+from floodsystem.plot import *
+from floodsystem.station import MonitoringStation
+from floodsystem.stationdata import build_station_list
+
+#NOT YET COMPLETED TEST
+#def test_plot_water_levels():
+    #Create list of seven example stations
+#    s_id = "test-s-id"
+#    m_id = "test-m-id"
+#    label = "some station"
+#    trange = (-2.3, 3.4445)
+#    river = "River X"
+#    coord = (100.2053, 87.1219)
+#    town = "My Town"
+#    testList = []
+#    for i in range(7):
+#        testList.append(MonitoringStation(s_id, m_id, label, coord,trange, river, town))
